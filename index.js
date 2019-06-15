8301a129471b8e3477e9ce02480123aa793e108b
Isaza156 <jonathan.isaza@hotmail.com>
2019-06-15 16:43:42 -0500

    act

---
+++ index.js
@@ -0,0 +1,9 @@
+var metros = 2
+var kilometros = 0, decimetros = 0
+if (metros % 2 == 0){
+kilometros = metros/100
+console.log("kilometros " + kilometros)
+}else{
+decimetros = metros * 10
+console.log("Decimetros " + decimetros)
+}
